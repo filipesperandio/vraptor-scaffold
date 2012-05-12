fb6650c9f13107fb257f4959068236383b2a70e1
Filipe Gomes Esperandio <filipesperandio@gmail.com>
2012-05-12 16:15:12 -0300

    javascript path

--- lib/vraptor-scaffold/configuration.rb
+++ lib/vraptor-scaffold/configuration.rb
@@ -4,6 +4,7 @@ class Configuration
   TEST_SRC = "src/test/java"
   TEST_RESOURCES = "src/test/resources"
   WEB_APP = "src/main/webapp"
+  JAVASCRIPT = "#{WEB_APP}/javascripts"
   WEB_INF = "#{WEB_APP}/WEB-INF"
   META_INF = "#{WEB_APP}/META-INF"
   FILENAME = "#{META_INF}/vraptor-scaffold.properties"
